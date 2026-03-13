70bee5f37a4a5a0e8e7a34590c3a370bd4a21051
Joe Francis <jafrancis@comcast.net>
2026-03-13 19:06:27 -0400

    Improve swagger support

--- ArchiForge.Api/Program.cs
+++ ArchiForge.Api/Program.cs
@@ -16,8 +16,12 @@ namespace ArchiForge.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "ArchiForge API", Version = "v1" });
+            });
 
             builder.Services.AddSingleton<SqlConnectionFactory>();
             builder.Services.AddHealthChecks()
@@ -51,6 +55,8 @@ namespace ArchiForge.Api
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ArchiForge API v1"));
             }
 
             app.UseHttpsRedirection();
