0716b7c1190be6d37040239158f05887e9b39b77
Joe Francis <jafrancis@comcast.net>
2026-06-28 21:25:27 -0400

    Fix migration 265 compile error: drop UPDATE before column exists.

--- ArchLucid.Persistence/Scripts/ArchLucid.sql
+++ ArchLucid.Persistence/Scripts/ArchLucid.sql
@@ -9141,16 +9141,11 @@ BEGIN
     ALTER TABLE dbo.RecommendationRecords
         ADD SourceEvidenceLinksJson NVARCHAR(MAX) NOT NULL
             CONSTRAINT DF_RecommendationRecords_SourceEvidenceLinksJson DEFAULT (N'[]');
-
-    UPDATE dbo.RecommendationRecords
-    SET SourceEvidenceLinksJson = N'[]'
-    WHERE SourceEvidenceLinksJson IS NULL;
-
-    ALTER TABLE dbo.RecommendationRecords DROP CONSTRAINT DF_RecommendationRecords_SourceEvidenceLinksJson;
 END;
 GO
 
 IF OBJECT_ID(N'dbo.RecommendationRecords', N'U') IS NOT NULL
+   AND COL_LENGTH(N'dbo.RecommendationRecords', N'SourceEvidenceLinksJson') IS NOT NULL
    AND NOT EXISTS (SELECT 1 FROM sys.check_constraints WHERE name = N'CK_RecommendationRecords_SourceEvidenceLinksJson_IsJson')
    AND NOT EXISTS (SELECT 1 FROM dbo.RecommendationRecords AS t WHERE ISJSON(t.SourceEvidenceLinksJson) <> 1)
 BEGIN
