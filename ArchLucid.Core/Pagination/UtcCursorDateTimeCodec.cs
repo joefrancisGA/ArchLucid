ff1510913918d36cd2128fbbeca39427895ebc54
Joe Francis <joefrancisGA@users.noreply.github.com>
2026-09-24 17:59:30 -0400

    fix: convert local cursor timestamps to UTC

--- ArchLucid.Core/Pagination/UtcCursorDateTimeCodec.cs
+++ ArchLucid.Core/Pagination/UtcCursorDateTimeCodec.cs
@@ -6,7 +6,9 @@ namespace ArchLucid.Core.Pagination;
 internal static class UtcCursorDateTimeCodec
 {
     public static string FormatRoundTripUtc(DateTime dateTime) =>
-        DateTime.SpecifyKind(dateTime, DateTimeKind.Utc).ToString("o");
+        (dateTime.Kind == DateTimeKind.Unspecified
+            ? DateTime.SpecifyKind(dateTime, DateTimeKind.Utc)
+            : dateTime.ToUniversalTime()).ToString("o");
 
     public static DateTime NormalizeToUtc(DateTime dateTime) =>
         dateTime.Kind is DateTimeKind.Utc
